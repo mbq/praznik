1b1b4c89225d1aca79cc996f07e5076f1e72bdf9
Miron B. Kursa <m@mbq.me>
2017-11-14 02:14:31 +0100

    Revert "More CMI shortcuts"
    This reverts commit 8dfb004b3a8587f70d3ce38ae6ec84e0d3c11559.

--- src/cmi.h
+++ src/cmi.h
@@ -9,9 +9,6 @@ SEXP C_CMI(SEXP X,SEXP Y,SEXP K){
  int nyz,nz,ncyz,ncz,nbyz=0,nbz=0,
   *yz=(int*)R_alloc(sizeof(int),n*6),
   *z=yz+n,*cyz=z+n,*cz=cyz+n,*byz=cz+n,*bz=byz+n;
- //For optimisation, last CMI record is also useful (I(X;Y)>=I(X;Y|Z) for any Z
- double *cmi=(double*)R_alloc(sizeof(double),m);
- for(int e=0;e<m;e++) cmi[e]=INFINITY;
 
  //For answer (we don't know the length a priori, CMI can become 0 at any time)
  double *_score=(double*)R_alloc(sizeof(double),k);
@@ -28,16 +25,16 @@ SEXP C_CMI(SEXP X,SEXP Y,SEXP K){
   int dyz=0,dz=0;
   for(int ee=0;ee<m;ee++){
    if(!x[ee]) continue;
-   if(cmi[ee]<bs) continue; //Updated CMI can be only lower
    ncyz=fillHt(ht,n,nx[ee],x[ee],nyz,yz,cyz,cX,dyz?NULL:cYZ,1); dyz=1;
-   cmi[ee]=miHt(ht,cX,cYZ);
+   double miX_YZ=miHt(ht,cX,cYZ);
    //Shortcut: miX_Z>0, hence final CMI won't get higher, and Ht takes a bit of time
-   if(cmi[ee]<=bs) continue;
+   if(miX_YZ<=bs) continue;
    ncz=fillHt(ht,n,nx[ee],x[ee],nz,z,cz,NULL,dz?NULL:cZ,1); dz=1;
-   cmi[ee]-=miHt(ht,cX,cZ);
+   double miX_Z=miHt(ht,cX,cZ);
+   double cmi=miX_YZ-miX_Z;
 
-   if(cmi[ee]>bs){
-    bs=cmi[ee]; bi=ee;
+   if(cmi>bs){
+    bs=cmi; bi=ee;
     nbyz=ncyz; nbz=ncz;
     ISWAP(byz,cyz);
     ISWAP(bz,cz);
