84089e220075adb2df75e1ac9ee3938ac9df74a4
Miron Bartosz Kursa <m@mbq.me>
2017-12-25 22:59:10 +0100

    Even less spawns

--- src/cmim.h
+++ src/cmim.h
@@ -21,42 +21,47 @@ SEXP C_CMIM(SEXP X,SEXP Y,SEXP K){
  //Time for an actual CMIM
  double *ms=(double*)R_alloc(sizeof(double),m); //Minimal score
  for(int e=0;e<m;e++) ms[e]=INFINITY;
- int *wy=(int*)R_alloc(sizeof(int),n*(nt+1)),*cXc=wy+n,*cWY=cY;
+ int *wy=(int*)R_alloc(sizeof(int),n*(nt+1)),*cXc=wy+n,*cWY=cY,nwy;
 
+ #pragma omp parallel
  for(int e=1;e<k;e++){
-  bs=-INFINITY;
-  int nwy=fillHt(hta[0],n,nw,w,ny,y,wy,NULL,NULL,1);
-  for(int ee=0;ee<nwy;ee++) cWY[ee]=hta[0]->cnt[ee].c;
-  #pragma omp parallel
+  #pragma omp single
   {
-   double tbs=-INFINITY;
-   int tbi=-1,tn=omp_get_thread_num(),*cX=cXc+(tn*n),*cW=ctmp+(tn*n),dw=0;
-   struct ht *ht=hta[tn];
-   #pragma omp for
-   for(int ee=0;ee<m;ee++){
-    //Ignore attributes already selected
-    if(!x[ee]) continue;
+   bs=-INFINITY;
+   nwy=fillHt(hta[0],n,nw,w,ny,y,wy,NULL,NULL,1);
+   for(int ee=0;ee<nwy;ee++) cWY[ee]=hta[0]->cnt[ee].c;
+  }
+  double tbs=-INFINITY;
+  int tbi=-1,tn=omp_get_thread_num(),*cX=cXc+(tn*n),*cW=ctmp+(tn*n),dw=0;
+  struct ht *ht=hta[tn];
+  #pragma omp for
+  for(int ee=0;ee<m;ee++){
+   //Ignore attributes already selected
+   if(!x[ee]) continue;
 
-    //WY x[ee] with wy making first part of cmi
-    fillHt(ht,n,nx[ee],x[ee],nwy,wy,NULL,cX,NULL,1);
-    double cmi=miHt(ht,cX,cWY);
-    //WY x[ee] with w to make second part of CMI
-    fillHt(ht,n,nw,w,nx[ee],x[ee],NULL,dw?NULL:cW,NULL,0); dw=1;
-    cmi-=miHt(ht,cW,cX);
-    ms[ee]=(ms[ee]<cmi)?ms[ee]:cmi;
+   //WY x[ee] with wy making first part of cmi
+   fillHt(ht,n,nx[ee],x[ee],nwy,wy,NULL,cX,NULL,1);
+   double cmi=miHt(ht,cX,cWY);
+   //WY x[ee] with w to make second part of CMI
+   fillHt(ht,n,nw,w,nx[ee],x[ee],NULL,dw?NULL:cW,NULL,0); dw=1;
+   cmi-=miHt(ht,cW,cX);
+   ms[ee]=(ms[ee]<cmi)?ms[ee]:cmi;
 
-    if(ms[ee]>tbs){
-     tbs=ms[ee]; tbi=ee;
-    }
-   }
-   #pragma omp critical
-   if(tbs>bs){
-    bs=tbs;
-    bi=tbi;
+   if(ms[ee]>tbs){
+    tbs=ms[ee]; tbi=ee;
    }
   }
-  w=x[bi]; nw=nx[bi]; x[bi]=NULL;
-  score[e]=bs; idx[e]=bi+1;
+  #pragma omp critical
+  if(tbs>bs){
+   bs=tbs;
+   bi=tbi;
+  }
+  #pragma omp barrier
+  #pragma omp single
+  {
+   w=x[bi]; nw=nx[bi]; x[bi]=NULL;
+   score[e]=bs; idx[e]=bi+1;
+  }
  }
 
  UNPROTECT(1);
