4a870766ca3bedd424956f46c6eb2e83bfed202b
Miron B. Kursa <m@mbq.me>
2017-12-26 18:55:06 +0100

    Hash table tweaks
    Even more eager to degenerate into a look-up table.

--- src/ht.h
+++ src/ht.h
@@ -26,11 +26,68 @@ struct ht* R_allocHt(int N){
 }
 
 uint32_t static inline fillHt(struct ht *Q,int N,int nA,int *a,int nB,int *b,int *mix,int *cA,int *cB,int mixOff){
- //Investigate special cases when ht is not needed, i.e. nA/nB=1, nA/nB=N. nA/nB=0 /probably same as =1/ ~> probably not for this function
- if(!b){
-  b=a;
-  nB=nA;
+ //Investigate the mode of HT; is it really needed?
+ 
+ if(nA*nB<N){
+  //OK, HT not needed, just use it as a table; map is not needed, we just 
+  int Neff=nA*nB;
+
+  //Initiate
+  uint32_t nAB=0;
+  if(cA) for(int e=0;e<nA;e++) cA[e]=0;
+  if(cB) for(int e=0;e<nB;e++) cB[e]=0;
+
+  if(!mix){
+   //Even deeper shortcut, no map at all
+   Q->nAB=Neff;
+   for(int e=0;e<Neff;e++) Q->cnt[e].c=0;
+   for(int e=0;e<N;e++){
+    uint32_t _a=a[e]-1;
+    uint32_t _b=b[e]-1;
+    uint64_t _ab=MAKE_AB(_a,_b);
+    uint32_t hab=_a+_b*nA;
+    Q->cnt[hab].c++;
+    Q->cnt[hab].ab=_ab;
+    if(cA) cA[_a]++;
+    if(cB) cB[_b]++;
+   }
+
+   return(Neff);
+  }
+  
+  //Zero the map
+  for(int e=0;e<Neff;e++) Q->map[e]=NULL;
+
+  //Fill HT
+  for(int e=0;e<N;e++){
+   uint32_t _a=a[e]-1;
+   uint32_t _b=b[e]-1;
+   uint64_t _ab=MAKE_AB(_a,_b);
+   uint32_t hab=_a+_b*nA;
+
+   struct hte **E=Q->map+hab;
+
+   if(!*E){
+    //Empty cell found
+    Q->cnt[nAB].ab=_ab;
+    Q->cnt[nAB].c=0;
+    *E=Q->cnt+nAB;
+    nAB++;
+   }
+
+   //Count this combination
+   (*E)->c++;
+   if(cA) cA[_a]++;
+   if(cB) cB[_b]++;
+
+   //TODO: Tautology here
+   if(mix) mix[e]=(*E-Q->cnt)+mixOff;
+  }
+  Q->nAB=nAB;
+
+  return(nAB);
  }
+ 
  //Zero HT
  uint32_t nAB=0;
  for(int e=0;e<N;e++) Q->map[e]=NULL;
@@ -42,26 +99,26 @@ uint32_t static inline fillHt(struct ht *Q,int N,int nA,int *a,int nB,int *b,int
   uint32_t _a=a[e]-1;
   uint32_t _b=b[e]-1;
   uint64_t _ab=MAKE_AB(_a,_b);
-  uint32_t hab=(nA*nB<N)?(_a+_b*nA):((_a^_b)%N);
+  uint32_t hab=(_a^_b)%N; //TODO: Real hash?
 
   struct hte **E=Q->map+hab;
-  for(;E[0] && E[0]->ab!=_ab;E=&(E[0]->nxt));
+  for(;(*E)&&(*E)->ab!=_ab;E=&((*E)->nxt));
 
-  if(!E[0]){
+  if(!*E){
    //Empty cell found
    Q->cnt[nAB].ab=_ab;
    Q->cnt[nAB].nxt=NULL;
    Q->cnt[nAB].c=0;
-   E[0]=Q->cnt+nAB;
+   *E=Q->cnt+nAB;
    nAB++;
   }
 
   //Count this combination
-  E[0]->c++;
+  (*E)->c++;
   if(cA) cA[_a]++;
   if(cB) cB[_b]++;
 
-  if(mix) mix[e]=(E[0]-Q->cnt)+mixOff;
+  if(mix) mix[e]=(*E-Q->cnt)+mixOff;
  }
  Q->nAB=nAB;
 
