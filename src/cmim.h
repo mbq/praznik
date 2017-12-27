b0d86737231a647b34fce8cdc43df3018846412e
Miron B. Kursa <m@mbq.me>
2017-12-27 01:24:40 +0100

    Fleuret algorithm in CMIM

--- src/cmim.h
+++ src/cmim.h
@@ -1,52 +1,60 @@
 //This is exactly the same as IF, hence the same implementation
-//TODO: Use Fleuret's trick to speed it up
 SEXP C_CMIM(SEXP X,SEXP Y,SEXP K){
  int n,k,m,ny,*y,*nx,**x;
  int nt=omp_get_max_threads();
  struct ht *hta[nt];
  prepareInput(X,Y,K,hta,&n,&m,&k,&y,&ny,&x,&nx,nt);
 
- double bs=0.; int bi=0,*cY,*ctmp;
- initialMiScan(hta,n,m,y,ny,x,nx,&cY,&ctmp,NULL,&bs,&bi,nt);
+ double bs=0.,*ms=(double*)R_alloc(sizeof(double),m);
+ int bi=0,*cYc,*ctmp;
+ initialMiScan(hta,n,m,y,ny,x,nx,&cYc,&ctmp,ms,&bs,&bi,nt);
  if(bs==0) return(makeAns(0,NULL,NULL));
  
- //Save selected X as W and discard from further consideration
- int* w=x[bi],nw=nx[bi]; x[bi]=NULL;
-
- //Yet put it as a first selected attribute
+ int *lk=(int*)R_alloc(sizeof(int),m),
+  **w=(int**)R_alloc(sizeof(int*),k),
+  *nw=(int*)R_alloc(sizeof(int),k),
+  *cW=(int*)R_alloc(sizeof(int),n*k),
+  *wy=(int*)R_alloc(sizeof(int),n*k),
+  *nwy=(int*)R_alloc(sizeof(int),k),
+  *cWY=(int*)R_alloc(sizeof(int),n*k);
+ w[0]=x[bi]; nw[0]=nx[bi]; x[bi]=NULL;
+ for(int e=0;e<m;e++) lk[e]=0;
  double *score; int *idx;
  SEXP Ans; PROTECT(Ans=makeAns(k,&score,&idx));
  score[0]=bs; idx[0]=bi+1;
  
- //Time for an actual CMIM
- double *ms=(double*)R_alloc(sizeof(double),m); //Minimal score
- for(int e=0;e<m;e++) ms[e]=INFINITY;
- int *wy=(int*)R_alloc(sizeof(int),n*(nt+1)),*cXc=wy+n,*cWY=cY,nwy;
-
  #pragma omp parallel
  for(int e=1;e<k;e++){
   #pragma omp single
   {
    bs=-INFINITY;
-   nwy=fillHt(hta[0],n,nw,w,ny,y,wy,NULL,NULL,1);
-   for(int ee=0;ee<nwy;ee++) cWY[ee]=hta[0]->cnt[ee].c;
+   int off=n*(e-1);
+   nwy[e-1]=fillHt(hta[0],n,nw[e-1],w[e-1],ny,y,wy+off,cW+off,NULL,1);
+   for(int ee=0;ee<nwy[e-1];ee++) (cWY+off)[ee]=hta[0]->cnt[ee].c;
   }
   double tbs=-INFINITY;
-  int tbi=-1,tn=omp_get_thread_num(),*cX=cXc+(tn*n),*cW=ctmp+(tn*n),dw=0;
+  int tbi=-1,tn=omp_get_thread_num(),*cX=cYc+(tn*n);
   struct ht *ht=hta[tn];
   #pragma omp for
   for(int ee=0;ee<m;ee++){
-   //Ignore attributes already selected
-   if(!x[ee]) continue;
+   //Ignore attributes already selected or with no chance to be max
+   if(!x[ee] || tbs>ms[ee]) continue;
+   
+   //Push forward towards e
+   for(;lk[ee]<e;lk[ee]++){
+    int ew=lk[ee],off=ew*n;
+    //WY x[ee] with wy making first part of cmi
+    fillHt(ht,n,nx[ee],x[ee],nwy[ew],wy+off,NULL,cX,NULL,0);
+    double cmi=miHt(ht,cX,cWY+off);
+    //WY x[ee] with w to make second part of CMI
+    fillHt(ht,n,nw[ew],w[ew],nx[ee],x[ee],NULL,NULL,NULL,0); 
+    cmi-=miHt(ht,cW+off,cX);
+    ms[ee]=(ms[ee]<cmi)?ms[ee]:cmi;
+    //Maybe it is already eliminated?
+    if(tbs>ms[ee]) break;
+   }
 
-   //WY x[ee] with wy making first part of cmi
-   fillHt(ht,n,nx[ee],x[ee],nwy,wy,NULL,cX,NULL,1);
-   double cmi=miHt(ht,cX,cWY);
-   //WY x[ee] with w to make second part of CMI
-   fillHt(ht,n,nw,w,nx[ee],x[ee],NULL,dw?NULL:cW,NULL,0); dw=1;
-   cmi-=miHt(ht,cW,cX);
-   ms[ee]=(ms[ee]<cmi)?ms[ee]:cmi;
-
+   //Check again
    if(ms[ee]>tbs){
     tbs=ms[ee]; tbi=ee;
    }
@@ -59,7 +67,7 @@ SEXP C_CMIM(SEXP X,SEXP Y,SEXP K){
   #pragma omp barrier
   #pragma omp single
   {
-   w=x[bi]; nw=nx[bi]; x[bi]=NULL;
+   w[e]=x[bi]; nw[e]=nx[bi]; x[bi]=NULL;
    score[e]=bs; idx[e]=bi+1;
   }
  }
