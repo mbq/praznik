5270bd91b272800dc7e7bf75d818c53be9ba282a
Miron B. Kursa <m@mbq.me>
2017-12-30 15:40:23 +0100

    Probably a better idea

--- src/praznik.c
+++ src/praznik.c
@@ -8,7 +8,9 @@
 #ifdef _OPENMP
  #include <omp.h>
 #else
- #include "fake-omp.h"
+ #define omp_get_thread_num() 0
+ #define omp_get_max_threads() 1
+ #define omp_set_num_threads(x)
 #endif
 
 //Hash table
