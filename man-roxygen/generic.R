2718dc000a3403cbb2d1a8a2741037183e227670
Miron B. Kursa <m@mbq.me>
2017-12-30 14:32:31 +0100

    Documentation updates

--- man-roxygen/generic.R
+++ man-roxygen/generic.R
@@ -4,7 +4,7 @@
 #' \code{NA}s are not allowed.
 #' @param k Number of attributes to select.
 #' Must not exceed \code{ncol(X)}.
-#' @return A list with two elements: \code{selection}, a vector of names of the selected features in the selection order (note that it may be shorter than \code{k}), and \code{scores}, a vector of feature scores.
+#' @return A list with two elements: \code{selection}, a vector of names of the selected features in the selection order (note that it may be shorter than \code{k}), and \code{scores}, a vector of corresponding feature scores.
 #' @note The method requires input to be discrete to use empirical estimators of distribution, and, consequently, information gain or entropy.
 #' To allow smoother user experience, praznik automatically coerces non-factor vectors in \code{X} and \code{Y}, which requires additional time and space and may yield confusing results -- the best practice is to convert data to factors prior to feeding them in this function.
 #' Real attributes are cut into about 10 equally-spaced bins, following the heuristic often used in literature.
